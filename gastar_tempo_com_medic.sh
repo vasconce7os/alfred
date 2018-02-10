527d18aff2e4c8c29f427ab534cd3faccae35d78
Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2018-02-10 10:06:52 -0400

    Create program to prepare workspace end tools

---
+++ gastar_tempo_com_medic.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+echo "Hello Wolrd"
+echo "Opening atom..."
+atom ~/public_html/newSaudeSystem
+
+echo "Opening eclipse..."
+/home/vasconcelos/eclipse/jee-latest-released/eclipse/eclipse -data $HOME/eclipse-workspace/ &
+
+echo "Starting JavaScript on back-end..."
+node ~/public_html/newSaudeSystem/medicsystem-web/src/index.js &
+
